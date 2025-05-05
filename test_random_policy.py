6d0972f44fd977935eb5943be164158c1e4404e0
Esteban Coronado Ortiz <estebanc@datup.ai>
2025-05-06 01:28:19 +0200

    Fix: modifications on render logic, not rendering, recording

--- test_random_policy.py
+++ test_random_policy.py
@@ -13,6 +13,7 @@
                 - What is an action here?
 """
 import pdb
+import os
 
 import  gym
 from gym.wrappers import RecordVideo
@@ -24,30 +25,31 @@ from env.custom_hopper import *
 def main():
 	env_base = gym.make('CustomHopper-source-v0') #render mode rgb_array to capture frames
 	# env = gym.make('CustomHopper-target-v0')
-	video_folder = "./videos"
+	video_folder = "./videos_hopper_gym021"
+	os.makedirs(video_folder, exist_ok=True) # Crear carpeta si no existe
 	# Record every 100 episodes
 	episode_trigger = lambda episode_id: episode_id % 100 == 0
 
-	print('State space:', env.observation_space) # state-space
-	print('Action space:', env.action_space) # action-space
-	print('Dynamics parameters:', env.get_parameters()) # masses of each link of the Hopper
+	print('State space:', env_base.observation_space) # state-space
+	print('Action space:', env_base.action_space) # action-space
+	print('Dynamics parameters:', env_base.get_parameters()) # masses of each link of the Hopper
 
 	n_episodes = 500
-	render = True
+	#render = True ## ------not necesary using RecordVideo ----
 	env = RecordVideo(env_base, video_folder=video_folder, episode_trigger=episode_trigger)
 
 	for episode in range(n_episodes):
 		done = False
 		state = env.reset()	# Reset environment to initial state
-
+		step_count = 0
 		while not done:  # Until the episode is over
 
 			action = env.action_space.sample()	# Sample random action
 		
 			state, reward, done, info = env.step(action)	# Step the simulator to the next timestep
-
-			if render:
-				env.render(mode='rgb_array')
+			#------- replaced by record video -----
+			#if render:
+			#	env.render(mode='rgb_array')
 
 	env.close()
 
